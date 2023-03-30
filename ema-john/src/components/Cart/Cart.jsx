404da450a50165b7cb46163809ef13ac0deaea67
Abir Khan <abirkhan.frontend.dev24@gmail.com>
2023-03-31 01:05:00 +0600

    Project Done

--- ema-john/src/components/Cart/Cart.jsx
+++ ema-john/src/components/Cart/Cart.jsx
@@ -8,9 +8,14 @@ const Cart = (props) => {
 
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart) {
-        totalPrice = totalPrice + product.price;
+
+        product.quantity = product.quantity || 1
+
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
 
     const tax = (totalPrice * 7) / 100;
@@ -21,7 +26,7 @@ const Cart = (props) => {
         <div className='cart'>
             <h2>Order Summary</h2>
             <div className="order-info">
-                <p>Selected Items : {cart.length}</p>
+                <p>Selected Items : {quantity}</p>
                 <p>Total Price : $ {totalPrice}</p>
                 <p>Total Shipping Charge : $ {totalShipping}</p>
                 <p>Tax : $ {tax.toFixed(2)}</p>
